96ed32c3e04916494331337b863312b9c11e6191
elmmaple <zowu059438erth@gmail.com>
2023-09-30 13:57:51 +0800

    modify - rich traceback

--- lib/rich_lib.py
+++ lib/rich_lib.py
@@ -1,8 +1,14 @@
+from config import RICH_CONFIG
 from rich.console import Console
 from rich.theme import Theme
-from config import RICH_CONFIG
+from rich.traceback import install
+install()
 
 def print(text, style = ''):
     CUSTOM_THEME = Theme(RICH_CONFIG['CUSTOM_THEME'])
     console = Console(theme = CUSTOM_THEME)
     console.print(text, style = style)
+    
+def console(text):
+    console = Console()
+    console.log(text, log_locals = True)
